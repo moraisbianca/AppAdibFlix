24b8b44bb80eda4b3c38c74bfa518a3ed8328691
Usuario <Usuario@CPD02-M17>
2022-04-13 15:51:30 -0300

    Adicionar arquivos de projeto.

---
+++ AppAdibFlix/AppAdibFlix/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppAdibFlix
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAventura_Clicked(object sender, EventArgs e)
+        {
+            try // "Vai tentar rodar o código abaixo" 
+            {
+                await Navigation.PushAsync(new Categorias.Aventura()); // navegar para a página da categoria aventura
+
+            } catch(Exception ex) // "Caso haja alguma excessão (erro)"
+            {
+                await DisplayAlert("Ops", ex.Message, "OK"); // "ex.Message" mostra a mensagem de erro
+            }
+        }
+
+        private async void btnComedia_Clicked(object sender, EventArgs e)
+        {
+            try 
+            {
+                await Navigation.PushAsync(new Categorias.Comedia());
+            } catch(Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void btnDrama_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new Categorias.Drama());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void btnTerror_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Navigation.PushAsync(new Categorias.Terror());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
